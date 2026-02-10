b7bc0a9ac999b4beb0240c09199115b51f1105fc
Deepak <nanimudhiraj45@gmail.com>
2026-02-10 17:24:52 -0600

    Add responce time

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const REGION = process.env.AWS_REGION || 'local';
 
-app.get('/', (req, res) => {
-  res.send(`Hello from ${REGION}!`);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
+
+app.get('/api/ping', (req, res) => {
+  res.send(REGION);
+});
+
+app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
